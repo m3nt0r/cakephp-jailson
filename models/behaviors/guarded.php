2cfcc0246847b106a145860e26c9f235df2b89f3
Kjell Bublitz <m3nt0r.de@gmail.com>
2010-09-20 14:09:47 +0200

    starting new behavior: Guarded. - control access on model level

---
+++ models/behaviors/guarded.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * Jailson for CakePHP
+ *   Access Control Plugin
+ * 
+ * @category CakePHP
+ * @author Kjell Bublitz <m3nt0r.de@gmail.com>
+ * @package plugins.jailson
+ * @subpackage plugins.jailson.behaviors
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @link http://github.com/m3nt0r/cakephp-jailson Repository/Docs
+ * @copyright (c) 2010, Kjell Bublitz (http://cakealot.com)
+ */
+/**
+ * Jailson - Guarded Behavior
+ * 
+ * @category CakePHP
+ * @author Kjell Bublitz <m3nt0r.de@gmail.com>
+ * @package plugins.jailson
+ * @subpackage plugins.jailson.behaviors
+ */
+class GuardedBehavior extends ModelBehavior {
+	
+	
+}
